df9201cd420057fe448948f90e0a2b8ccf45726f
Nicolás Pérez Oportus <nicolas.perez.oportus96@gmail.com>
2021-08-23 17:38:50 -0400

    Componente VectorTilesLayer

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,15 +1,15 @@
 //imports
 import React, { useContext } from 'react';
-import { Map, TileLayer, LayersControl, LayerGroup, withLeaflet} from 'react-leaflet';
-import L, { CRS } from 'leaflet';
+import { Map, TileLayer, LayersControl, LayerGroup, } from 'react-leaflet';
+import { CRS } from 'leaflet';
 import { FeatureContext } from './FeatureContext';
-import VectorGridDefault from "react-leaflet-vectorgrid";
+import VectorTilesLayer from './VectorTilesLayer';
 
 //FUNCTIONAL COMPONENT
 export default function MapComponent({ data }) {
     const { feature, elaMethod } = useContext(FeatureContext);
-    const [featureValue, setFeatureValue] = feature;
-    const [ elaMethodValue, setElaMethodValue] = elaMethod;
+    const [, setFeatureValue] = feature;
+    const [ , setElaMethodValue] = elaMethod;
 
     //funcion para añadir datos del poligono seleccionado al context.
     function getFeatureData(featureData) {
@@ -17,71 +17,7 @@ export default function MapComponent({ data }) {
         setFeatureValue(featureData);
     }
 
-    //estilos para los vectores  
-    const vectorStyles = {
-        ING_Montana: {
-            minZoom: 12, 
-            stroke: true,
-            color: 'black',
-            weight: 1.3,
-            opacity: 0.5,
-            linejoin: 'round',
-            dashArray: '2, 3',
-            fill: true, 
-            fillColor: '#39d2ff',
-            fillOpacity: 0.5,
-        },
-        ING_Valle: {
-            stroke: true,
-            color: 'black',
-            weight: 1.3,
-            opacity: 0.5,
-            linejoin: 'round',
-            dashArray: '2, 3',
-            fill: true, 
-            fillColor: '#39ecbc',
-            fillOpacity: 0.5,
-        },
-        ING_Rocoso: {
-            stroke: true,
-            color: 'black',
-            weight: 1.3,
-            opacity: 0.5,
-            linejoin: 'round',
-            dashArray: '2, 3',
-            fill: true, 
-            fillColor: '#ffd501',
-            fillOpacity: 0.5,
-        },
-        ING_Glaciarete: {
-            stroke: true,
-            color: 'black',
-            weight: 1.3,
-            opacity: 0.5,
-            linejoin: 'round',
-            dashArray: '2, 3',
-            fill: true, 
-            fillColor: '#ac11ff',
-            fillOpacity: 0.5,
-        }
-    };
 
-    //opciones VectorGrid
-    const options = {
-        tolerance: 30, // 5 simplification tolerance (higher means simpler)
-        extent: 4096, //4096, // 4096 tile extent (both width and height)
-        buffer: 128, // 64 default 64tile buffer on each side
-        rendererFactory: L.svg.tile ,
-        type: "protobuf",
-        url: "http://34.121.165.39/teselas/ING_VT/{z}/{x}/{y}.pbf",
-        vectorTileLayerStyles: vectorStyles,
-        subdomains: "abcd",
-        key: "abcdefghi01234567890",
-        interactive: true,
-        zIndex: 401 
-    };
-
-    const VectorGrid = withLeaflet(VectorGridDefault);
     //JSX
     return (
         <Map center={[-34.238347, -70.250921]} zoom={8} minZoom={8} maxZoom={12} maxBounds={[[-35.494268, -70.735148], [-32.963408, -69.766694]]} crs={CRS.EPSG3857} >
@@ -120,7 +56,9 @@ export default function MapComponent({ data }) {
                     <TileLayer url="http://34.121.165.39/teselas/Label/{z}/{x}/{y}.png" tms={false} />
                 </LayersControl.Overlay>
 
-                <VectorGrid {...options}  onClick={ (e) => {getFeatureData(e.layer.properties);}  } />
+                <VectorTilesLayer url="http://34.121.165.39/teselas/ING_VT/{z}/{x}/{y}.pbf" clickHandler={(e) => getFeatureData(e.layer.properties)} />
+
+                
                 
             </LayersControl>
         </Map>
